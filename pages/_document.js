92fada7d5c6ce9a0fe01b0be63224599d178d12b
Muhammad Adil <adilsaeed31@gmail.com>
2020-04-11 13:35:49 +0300

    added font

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,16 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Montserrat"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
